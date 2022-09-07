2889c2d25874af851e3124a36046bacf834a917c
Emma Cohanim <emmacohanim@gmail.com>
2022-09-06 21:39:40 -0700

    commit tree 461484fa6786c2bf1b462cb2a0b429dad6e0dfe8

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,4 +1,8 @@
 class CreateStudents < ActiveRecord::Migration[6.1]
   def change
+    # use active record create table to create table with :name string
+    create_table :students do |t|
+      t.string :name
+    end
   end
 end
